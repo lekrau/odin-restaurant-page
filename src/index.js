68d0e4891a1829a60e216349346d60bc3db5d158
lekrau <206306997+lekrau@users.noreply.github.com>
2026-09-03 08:45:48 +0200

    Set up CSS loading with Webpack

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 console.log("index.js");
 
+import "./styles.css";
 import loadHomePage from "./home-page-load.js";
 import loadMenuPage from "./menu-page-load.js";
 import loadContactPage from "./contact-page-load.js";
--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,10 @@ export default {
     module: {
         rules: [
             {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
                 test: /\.html$/i,
                 use: ["html-loader"],
             },
